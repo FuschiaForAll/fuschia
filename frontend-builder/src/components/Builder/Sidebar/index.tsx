ec2b401d07236a7fc14128879af06b881e042f4f
Jeremy Blalock <jeremyjblalock@gmail.com>
2022-02-14 19:51:08 -0600

    Adds builder sidebar

--- frontend-builder/src/components/Builder/Sidebar/index.tsx
+++ frontend-builder/src/components/Builder/Sidebar/index.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 import styled from '@emotion/styled'
 import Paper from '@mui/material/Paper'
 import IconButton from '@mui/material/IconButton'
-//import Icon from '../../Shared/Icon'
+import Icon from '../../Shared/Icon'
 
-import LayersIcon from '@mui/icons-material/Layers'
-import BorderAllIcon from '@mui/icons-material/BorderAll'
+import AppsIcon from '@mui/icons-material/Apps'
+import FormatColorTextIcon from '@mui/icons-material/FormatColorText'
+import ImageIcon from '@mui/icons-material/Image'
 
 const Wrapper = styled.div`
   position: fixed;
@@ -33,24 +34,45 @@ const buttonStyles = {
 }
 
 const Item: React.FC = function Item({ children }) {
-  return <IconButton sx={buttonStyles}>{children}</IconButton>
+  return (
+    <IconButton color="primary" sx={buttonStyles}>
+      {children}
+    </IconButton>
+  )
 }
 
 const Sidebar: React.FC = function Sidebar() {
   return (
     <Wrapper>
-      <Paper elevation={12} sx={cardStyles}></Paper>
       <Paper elevation={12} sx={cardStyles}>
-        {/*
         <Item>
-          <Icon />
-        </Item>
-        */}
-        <Item>
-          <LayersIcon />
+          <AppsIcon />
         </Item>
         <Item>
-          <BorderAllIcon />
+          <FormatColorTextIcon />
+        </Item>
+        <Item>
+          <ImageIcon />
+        </Item>
+        <Item>
+          <Icon icon="shapes" />
+        </Item>
+        <Item>
+          <Icon icon="expandRight" />
+        </Item>
+        <Item>
+          <Icon icon="input" />
+        </Item>
+      </Paper>
+      <Paper elevation={12} sx={cardStyles}>
+        <Item>
+          <Icon icon="database" />
+        </Item>
+        <Item>
+          <Icon icon="styles" />
+        </Item>
+        <Item>
+          <Icon icon="analytics" />
         </Item>
       </Paper>
     </Wrapper>
--- frontend-builder/src/components/Shared/Icon/index.tsx
+++ frontend-builder/src/components/Shared/Icon/index.tsx
@@ -2,21 +2,37 @@ import React from 'react'
 import SvgIcon from '@mui/material/SvgIcon'
 
 import databaseIcon from './icons/database.svg'
+import analyticsIcon from './icons/analytics.svg'
+import stylesIcon from './icons/styles.svg'
+import shapesIcon from './icons/shapes.svg'
+import expandRightIcon from './icons/expand-right.svg'
+import inputIcon from './icons/input.svg'
 
-type IconType = 'database'
+type IconType =
+  | 'database'
+  | 'analytics'
+  | 'styles'
+  | 'shapes'
+  | 'expandRight'
+  | 'input'
 
 interface IconProps {
   icon: IconType
 }
 
-const icons: Record<IconType, SVGElement> = {
-  database: databaseIcon as any as SVGElement,
+const icons: Record<IconType, React.ComponentType> = {
+  database: databaseIcon as any as React.ComponentType,
+  analytics: analyticsIcon as any as React.ComponentType,
+  styles: stylesIcon as any as React.ComponentType,
+  shapes: shapesIcon as any as React.ComponentType,
+  expandRight: expandRightIcon as any as React.ComponentType,
+  input: inputIcon as any as React.ComponentType,
 }
 
 const Icon: React.FC<IconProps> = function Icon({ icon }) {
-  const svg = icons[icon]
+  const Svg = icons[icon]
 
-  return <SvgIcon inheritViewBox component={svg} />
+  return <SvgIcon inheritViewBox component={Svg} />
 }
 
 export default Icon
