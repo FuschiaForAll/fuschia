fe67db42b43be06a37b281cd7c8bac4c3196d353
Dave Diplock <dave@pragmaflow.com>
2022-05-05 17:40:36 -0400

    Updating env generation, script default, env defaults. Interactive will now read existing data

--- backend-builder/generate-env.ts
+++ backend-builder/generate-env.ts
@@ -1,6 +1,9 @@
 import { requiredConfigVars } from './src/utils/config.vars';
 import fs from 'fs';
 import * as readline from 'readline';
+require('dotenv').config();
+
+console.log("\n\x1b[31mBy default, `yarn generate-env` will generate an .env-sample file you can use as a template to create your own .env file. Using argument env=live will generate a blank .env file. Use argument interactive to fill out the required fields and generate the .env file.\x1b[0m");
 
 function askQuestion(query: string) {
     const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
@@ -27,8 +30,8 @@ if (args["env"] && args["env"] === "live") {
 (async () => {
     let proceed = false;
     if (fs.existsSync(`./${outputFilename}`) && !args['force']) {
-        const answer = await askQuestion(`The file \`${outputFilename}\` already exists. Would you like to overwrite? [y/N]`);
-        proceed = (answer && answer.trim() === "y" ? true : false);
+        const answer = await askQuestion(`The file \`${outputFilename}\` already exists. Would you like to overwrite? [y/N] `);
+        proceed = (answer && answer.trim().toLowerCase() === "y" ? true : false);
     } else {
         proceed = true;
     }
@@ -41,13 +44,17 @@ if (args["env"] && args["env"] === "live") {
     let inputValues: { [key: string]: string } = {};
     if (args["interactive"]) {
         for (const item of requiredConfigVars) {
-            let question = `Value for ${item.key}` + (item.note ? ` (${item.note})` : '') + (item.options ? `. Allowed values: \"${item.options.map(opt => opt.value).join("\", \"")}\"` : '') + ": ";
+            let thisDefault = (process.env[item.key] ? process.env[item.key] : (item.default ? item.default : ""));
+            let question = `Value for ${item.key}` + (item.note ? ` (${item.note})` : '') + (item.options ? `. Allowed values: \"${item.options.map(opt => opt.value).join("\", \"")}\"` : '') + (thisDefault ? ` [${thisDefault}]` : "") + ": ";
 
-            const answer = (await askQuestion(question)).trim();
+            let answer = (await askQuestion(question)).trim();
+            if ((!answer || answer === "") && thisDefault) {
+                answer = thisDefault;
+            }
             inputValues[item.key] = answer;
 
             if (item.options) {
-                const check = item.options.filter(item => item.value === answer);
+                const check = item.options.filter(item => item.value.toLowerCase() === answer.toLowerCase());
                 if (check.length) {
                     if (check[0].dependencies) {
                         for (const dependency of check[0].dependencies) {
@@ -63,20 +70,16 @@ if (args["env"] && args["env"] === "live") {
     const writeOutput: string[] = [];
     requiredConfigVars.forEach(item => {
         let output: string = `${item.key}=${(inputValues[item.key] ? inputValues[item.key] : "")}`;
-        if (item.note) {
-            output += ` #${item.note}`;
-        }
         if (!item.options) {
             writeOutput.push(output);
             return;
         }
-        output += `#Allowed values: ${item.options.map(opt => opt.value).join(", ")}`;
         writeOutput.push(output);
 
         item.options.forEach(subopt => {
             if (subopt.dependencies) {
                 subopt.dependencies.forEach(dependency => {
-                    writeOutput.push(`${dependency}=${(inputValues[dependency] ? inputValues[dependency] : "")} #Only required when using ${item.key}=${subopt.value}`);
+                    writeOutput.push(`${dependency}=${(inputValues[dependency] ? inputValues[dependency] : "")}`);
                 });
             }
         });
